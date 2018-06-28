7eac2bd1a17a856c7fcf034021fae53a0520066c
Jason Rivers <jason@jasonrivers.co.uk>
2018-06-28 11:23:10 +0100

    Only change permissions if the directory ownership is incorrect

--- overlay/scripts/bootstrap.sh
+++ overlay/scripts/bootstrap.sh
@@ -5,10 +5,11 @@ sed -i "s/CACHE_MEM_SIZE/${CACHE_MEM_SIZE}/"  /etc/nginx/sites-available/generic
 sed -i "s/CACHE_DISK_SIZE/${CACHE_DISK_SIZE}/" /etc/nginx/sites-available/generic.conf
 sed -i "s/CACHE_MAX_AGE/${CACHE_MAX_AGE}/"    /etc/nginx/sites-available/generic.conf
 
-echo "Checking permissions (This may take a long time if the permissions are incorrect on large caches)..."
-find /data \! -user ${WEBUSER} -exec chown ${WEBUSER}:${WEBUSER} '{}' +
-
-echo "Done. Starting caching server."
+if [ `stat -c '%U' /data/cache` != "${WEBUSER}" ] || [ `stat -c '%U' /data/logs` != "${WEBUSER}" ] || [ `stat -c '%U' /data/info` != "${WEBUSER}" ] ; then
+    echo "Checking permissions (This may take a long time if the permissions are incorrect on large caches)..."
+    find /data \! -user ${WEBUSER} -exec chown ${WEBUSER}:${WEBUSER} '{}' +
+    echo "Done. Starting caching server."
+fi
 
 /usr/sbin/nginx -t
 
