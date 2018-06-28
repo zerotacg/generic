441ed6840a3cb48c07cc74880da234cbf040107d
Jason Rivers <jason@jasonrivers.co.uk>
2018-06-28 10:05:59 +0100

    Take ownership of cache files at startup
    fixes #34

--- overlay/scripts/bootstrap.sh
+++ overlay/scripts/bootstrap.sh
@@ -5,6 +5,9 @@ sed -i "s/CACHE_MEM_SIZE/${CACHE_MEM_SIZE}/"  /etc/nginx/sites-available/generic
 sed -i "s/CACHE_DISK_SIZE/${CACHE_DISK_SIZE}/" /etc/nginx/sites-available/generic.conf
 sed -i "s/CACHE_MAX_AGE/${CACHE_MAX_AGE}/"    /etc/nginx/sites-available/generic.conf
 
+echo "Checking permissions..."
+chown -R ${WEBUSER}:${WEBUSER} /data/cache /data/info /data/logs
+echo "Done. Starting caching server."
 
 /usr/sbin/nginx -t
 
