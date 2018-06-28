d413353141966192f2542fce81ee08cf4fea5306
Jason Rivers <jason@jasonrivers.co.uk>
2018-06-28 11:08:42 +0100

    Use find rather than chown directly
    Tests showed this to be significantly faster to run than forcing a
    chown on all files in the directory

--- overlay/scripts/bootstrap.sh
+++ overlay/scripts/bootstrap.sh
@@ -5,8 +5,9 @@ sed -i "s/CACHE_MEM_SIZE/${CACHE_MEM_SIZE}/"  /etc/nginx/sites-available/generic
 sed -i "s/CACHE_DISK_SIZE/${CACHE_DISK_SIZE}/" /etc/nginx/sites-available/generic.conf
 sed -i "s/CACHE_MAX_AGE/${CACHE_MAX_AGE}/"    /etc/nginx/sites-available/generic.conf
 
-echo "Checking permissions..."
-chown -R ${WEBUSER}:${WEBUSER} /data/cache /data/info /data/logs
+echo "Checking permissions (This may take a long time if the permissions are incorrect on large caches)..."
+find /data \! -user ${WEBUSER} -exec chown ${WEBUSER}:${WEBUSER} '{}' +
+
 echo "Done. Starting caching server."
 
 /usr/sbin/nginx -t
