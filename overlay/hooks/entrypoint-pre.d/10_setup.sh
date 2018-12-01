7d3036407effd0f4c25579f66494052f08597954
VibroAxe <vibs@macrolevel.co.uk>
2018-12-01 17:13:59 +0000

    Added customisable worker processes environment

--- overlay/hooks/entrypoint-pre.d/10_setup.sh
+++ overlay/hooks/entrypoint-pre.d/10_setup.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -e
 
+echo "worker_processes ${NGINX_WORKER_PROCESSES};" > /etc/nginx/workers.conf
 sed -i "s/CACHE_MEM_SIZE/${CACHE_MEM_SIZE}/"  /etc/nginx/sites-available/00_generic.conf
 sed -i "s/CACHE_DISK_SIZE/${CACHE_DISK_SIZE}/" /etc/nginx/sites-available/00_generic.conf
 sed -i "s/CACHE_MAX_AGE/${CACHE_MAX_AGE}/"    /etc/nginx/sites-available/generic.conf.d/root/20_cache.conf
