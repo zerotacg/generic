6b3160d0adc02980d8d40441a7ddc64d5de410e2
VibroAxe <vibs@macrolevel.co.uk>
2019-02-14 11:10:50 +0000

    Added nginx checking and fast permissions check

--- overlay/hooks/entrypoint-pre.d/20_perms_check.sh
+++ overlay/hooks/entrypoint-pre.d/20_perms_check.sh
@@ -1,6 +1,14 @@
-#!/bin/sh
-set -e
+#!/bin/bash
+if [ -d "/data/cache/cache" ]; then
+	echo "Running fast permissions check"
+	ls -l /data/cache/cache | tail --lines=+2 | grep -v ${WEBUSER} > /dev/null
 
-echo "Checking permissions (This may take a long time if the permissions are incorrect on large caches)..."
-find /data \! -user ${WEBUSER} -exec chown ${WEBUSER}:${WEBUSER} '{}' +
-echo "Permisions ok"
+	if [[ $? -eq 0 || "$FORCE_PERMS_CHECK" == "true" ]]; then
+		echo "Doing full checking of permissions (This WILL take a long time on large caches)..."
+		find /data \! -user ${WEBUSER} -exec chown ${WEBUSER}:${WEBUSER} '{}' +
+		echo "Permissions ok"
+	else
+		echo "Fast permissions check successful, if you have any permissions error try running with -e FORCE_PERMS_CHECK = true"
+	fi
+
+fi
